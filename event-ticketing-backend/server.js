f3cba0f5b2638a0bb19752e23af55fa16b834935
omar emam <omarems46@gmail.com>
2025-05-12 13:08:26 +0300

    hello

--- event-ticketing-backend/server.js
+++ event-ticketing-backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-
+    
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
